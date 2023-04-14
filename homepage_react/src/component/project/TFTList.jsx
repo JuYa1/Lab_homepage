641d44ad117ffa8d6d839c507fb17ca1f558dd29
soju0816 <soju0816@cu.ac.kr>
2023-04-14 13:21:05 +0900

    "update"

--- homepage_react/src/component/project/TFTList.jsx
+++ homepage_react/src/component/project/TFTList.jsx
@@ -1,8 +1,28 @@
+// import React from "react";
+// import TFTCard from "./TFTCard";
+// import styles from "./tftList.module.css";
+
+// function TFTList({ data }) {
+//   return (
+//     <div className={styles.tftlist}>
+//       {data.map((_, __) => (
+//         <TFTCard data={_} key={__} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default TFTList;
+
 import React from "react";
 import TFTCard from "./TFTCard";
 import styles from "./tftList.module.css";
 
 function TFTList({ data }) {
+  if (!Array.isArray(data)) {
+    return <div>데이터가 없습니다.</div>;
+  }
+
   return (
     <div className={styles.tftlist}>
       {data.map((_, __) => (
